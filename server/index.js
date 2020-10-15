7a042898a849d5b8defdce1613a96d8788d3192a
brice <bricelehew@gmail.com>
2020-10-15 17:07:13 -0500

    move to environment vars

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const Filter = require("bad-words");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const app = express();
-const route = `mongodb://user:E3jMs33OymDkdoIG@sezzle-web-calculator-shard-00-00.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-01.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-02.urh1h.mongodb.net:27017/barks?ssl=true&replicaSet=atlas-qic6uj-shard-0&authSource=admin&retryWrites=true&w=majority`
+
+const route = `mongodb://${process.env.USER}:${process.env.AUTHKEY}@sezzle-web-calculator-shard-00-00.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-01.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-02.urh1h.mongodb.net:27017/barks?ssl=true&replicaSet=atlas-qic6uj-shard-0&authSource=admin&retryWrites=true&w=majority`
 const db = monk(route || "localhost/barker");
 const barks =db.get("barks");
 const filter = new Filter();
